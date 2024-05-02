dcee6a6cbbf1b48d8b7597053e10decfe2338231
Cilosan <NicoJMaier@t-online.de>
2024-05-02 09:03:06 +0200

    All Data
    Alle Daten Stand 02.05.24

---
+++ LoanB/CreditWebservice/src/main/java/com/example/creditWebservice/CreditWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.creditWebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditWebserviceApplication.class, args);
+	}
+
+}
